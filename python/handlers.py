b6b89b4b8e1b6cffbc12bdb985a9e6a1adc86a32
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-01-06 06:51:58 +0300

    feat: change overpay on detailed skin info

--- python/handlers.py
+++ python/handlers.py
@@ -20,7 +20,7 @@ def handle_inventory_list(flow: http.HTTPFlow, user) -> None:
             # Make new price
             new_price = item["price"]
             for overpay in overpays.values():
-                if overpay >= 0:
+                if overpay > 0:
                     new_price += overpay
                     total_cost += overpay
             item["price"] = new_price
@@ -42,3 +42,39 @@ def handle_inventory_list(flow: http.HTTPFlow, user) -> None:
     new_json_reponse = json.dumps(parsed_response)
     content_type = { "content-type": "application/json" }
     flow.response = http.Response.make(200, new_json_reponse, content_type)
+
+
+def handle_skin_info(flow: http.HTTPFlow, user) -> None:
+    raw_response = flow.response.content
+    parsed_response = json.loads(raw_response)
+
+    print("handle_skin_info")
+
+    try:
+        user_items = user["items"]
+
+        item_id = int(flow.request.query["id"])
+        custom_item_settings =  next(x for x in user_items if x["id"] == item_id)
+        custom_overpays = custom_item_settings["overpay"]
+
+        print(custom_overpays)
+
+        price = parsed_response["price"]
+
+        if not "overpay" in parsed_response.keys():
+            parsed_response["overpay"] = {}
+        
+        for (key, value) in custom_overpays.items():
+            if value > 0:
+                parsed_response["overpay"][key] = value
+                price += value
+
+        parsed_response["price"] = price
+
+        # Stringify new reponse and send it to the client
+        new_json_reponse = json.dumps(parsed_response)
+        content_type = { "content-type": "application/json" }
+        flow.response = http.Response.make(200, new_json_reponse, content_type)
+    except StopIteration:
+        print("except")
+        return None
--- python/proxy.py
+++ python/proxy.py
@@ -1,14 +1,19 @@
+from time import time
 from mitmproxy import http
-from handlers import *
 from helpers import *
+from handlers import *
 
 def response(flow: http.HTTPFlow) -> None:
     # Check client access to use proxy
     ip = flow.client_conn.address[0]
     user = find_user_by_ip(ip)
 
+    current_time = time() * 1000
+    print(current_time)
+    user_access_till = user["access_till"]
+
     # If client's IP is not in users list, show access error
-    if not user:
+    if not user or current_time > user_access_till:
         error_html = open("forbidden.html", 'r').read()
         content_type = { "Content-Type": "text/html" }
         flow.response = http.Response.make(200, error_html, content_type)
@@ -20,4 +25,8 @@ def response(flow: http.HTTPFlow) -> None:
         if "load_user_inventory" in flow.request.path:
             # TODO: Send inventory data to node server
             handle_inventory_list(flow, user)
+        
+        # Else if client requests info about exectly skin
+        if "skin_info" in flow.request.path:
+            handle_skin_info(flow, user)
 
